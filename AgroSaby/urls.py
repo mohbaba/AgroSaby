bcb782b107cc3cb5c682f3d94075a38e3e07940e
mohbaba <babamu09@gmail.com>
2023-03-13 16:48:40 +0100

    Project Setup

--- AgroSaby/urls.py
+++ AgroSaby/urls.py
@@ -18,5 +18,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('', include('core_app.urls')),
+    path('user_profiles/', include('user_profiles.urls')),
     path('admin/', admin.site.urls),
 ]
