fdd0e6d4310017ecffaf4597f05646268b63c008
mohbaba <babamu09@gmail.com>
2023-03-18 14:17:22 +0100

    Views for index page

--- core_app/views.py
+++ core_app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+def index(request):
+    
+    return render( 'index.html')
