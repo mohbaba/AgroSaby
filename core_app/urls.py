ffb36e60eaafd8cc9fd576100c03d9ba807b0f2c
mohbaba <babamu09@gmail.com>
2023-03-20 23:47:24 +0100

    Page Routing

--- core_app/urls.py
+++ core_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path , include
-from core_app.views import index
+from core_app.views import *
 
 urlpatterns = [
-    path('', index , name = 'home')
+    path('', index , name = 'home'),
+    path('about', about_us , name = 'about'),
+    path('gallery',gallery_page, name = 'gallery'),
+    path('contact_us', contact_page , name= 'contact'),
+    path('shop', shop_page, name= 'shop'),
+    
     
 ]
--- core_app/views.py
+++ core_app/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
     
     return render(request ,'index.html')
+
+def about_us(request):
+    
+    return render(request , 'about.html')
+
+
+def gallery_page(request):
+    return render (request, 'gallery.html')
+
+def contact_page(request):
+    return render(request, 'contact-us.html')
+
+def shop_page(request):
+    return render(request, 'shop.html')
