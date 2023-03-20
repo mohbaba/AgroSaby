3956c6fe910f8033a97bf810e48e48bad4349b72
mohbaba <babamu09@gmail.com>
2023-03-20 17:09:59 +0100

    Views for index page

--- AgroSaby/urls.py
+++ AgroSaby/urls.py
@@ -26,3 +26,6 @@ urlpatterns = [
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
