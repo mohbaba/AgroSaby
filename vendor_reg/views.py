574cc3cf91f28c541d1550572beb632c595d8097
mohbaba <babamu09@gmail.com>
2023-04-02 23:59:03 +0100

    Form method updates

--- vendor_reg/views.py
+++ vendor_reg/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
 # Create your views here.
 
 
 def tester(request):
-    return render(request, 'buyer_reg.html')
+    
+    if request.method == 'POST':
+        firstname = request['firstname']
+        lastname = request['lastname']
+        email = request['email']
+        password1 = request['passsword']
+        password2 = request['password2']
+        phone_no = request['phone']
+        acc_type = request['acc_type']
+        
+        if password1 == password2:
+            pass
+        else:
+            
+        
+        
+        
+    else:
+        return render(request, 'buyer_reg.html')
 
 
 def about_us(request):
