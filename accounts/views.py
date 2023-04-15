340cd7b7fdae27b7659ff21db5eacdba1bd83580
mohbaba <babamu09@gmail.com>
2023-04-15 23:27:07 +0100

    Adds Initial Value Retention to the fields after validation errors

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from core_app.urls import *
 from accounts.models import *
 from django.contrib import messages
 from django.http import HttpResponse
@@ -7,7 +8,7 @@ from django.http import HttpResponse
 
 
 def account(request):
-    
+    context = {}
     if request.method == 'POST':
         firstname = request.POST.get('firstname')
         username = request.POST.get('username')
@@ -17,67 +18,45 @@ def account(request):
         password2 = request.POST.get('password2')
         phone_no = request.POST.get('phone')
         acc_type = request.POST.get('acc_type')
+        
+        context = {
+                    'email': email,
+                    'username': username,
+                    'firstname': firstname,
+                    'lastname': lastname,
+                    'phone': phone_no,
+                    'acc_type': acc_type,
+                }
 
         
         if password1 == password2:
             if User.objects.filter(email = email).exists():
                 messages.info(request, 'Email Taken')
-                context = {
-                    'email': email,
-                    'username': username,
-                    'firstname': firstname,
-                    'lastname': lastname,
-                    'phone': phone_no,
-                    'acc_type': acc_type,
-                }
-                return redirect('buyer_reg.html', context)
+                # return redirect('buyer_reg.html', context)
+                return render(request, 'buyer_reg.html', context)
             elif User.objects.filter(username = username).exists():
                 messages.info(request, 'Username taken try another one ')
-                context = {
-                    'email': email,
-                    'username': username,
-                    'firstname': firstname,
-                    'lastname': lastname,
-                    'phone': phone_no,
-                    'acc_type': acc_type,
-                }
+                # return redirect('buyer_reg.html', context)
+                return render(request, 'buyer_reg.html', context)
             else:
                 user = User.objects.create_user(username = username,email=email, password=password1)
-                user.save()
-                acc = Accounts(firstname = firstname, lastname = lastname, phone_number = phone_no, user_type = acc_type)
+                acc = Accounts(firstname = firstname, lastname = lastname, phone_number = phone_no, user_type = acc_type, user =user)
                 acc.save()
                 
                 #log user in and direct to settings page
                 
                 # user_login = auth.authenticate(username=username, password=password1)
                 # auth.login(request,user_login)
-                
-                
-                context = {
-                    'email': email,
-                    'username': username,
-                    'firstname': firstname,
-                    'lastname': lastname,
-                    'phone': phone_no,
-                    'acc_type': acc_type,
-                }
-                return render(request, 'buyer_reg.html', context)
+                return redirect('home')
         else:
             messages.info(request, 'Passwords do not match!')
-            context = {
-                    'email': email,
-                    'username': username,
-                    'firstname': firstname,
-                    'lastname': lastname,
-                    'phone': phone_no,
-                    'acc_type': acc_type,
-                }
-            return redirect('buyer_reg.html',context)
+            return render(request, 'buyer_reg.html', context)
+            # return redirect('buyer_reg.html',context)
         
         
         
     else:
-        return render(request, 'buyer_reg.html')
+        return render(request, 'buyer_reg.html', context)
 
 
 def about_us(request):
