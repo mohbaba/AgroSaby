f1d1a33141a4ea2e03887db5a5bdf33e657b0d58
mohbaba <babamu09@gmail.com>
2023-04-18 17:13:46 +0100

    Adds Login fonctionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,7 @@ from accounts.views import *
 urlpatterns = [
     path('', account , name='registration'),
     path('register', account, name='buyer_registration'),
-    path('sign_in', sign_in, name= 'sign_in')
+    path('sign_in', sign_in, name= 'sign_in'),
+    path('logout', log_user_out, name= 'logout'),
+    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib.auth import authenticate , login , logout
 from core_app.urls import *
 from accounts.models import *
 from django.contrib import messages
@@ -61,5 +62,24 @@ def account(request):
 
 
 def sign_in(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request , username= username, password = password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Success!')
+            
+            return redirect('home')
+        else:
+            messages.error(request, 'User not found! Create new account')
+            return redirect('sign_in')
+    else:
+        return render(request , 'sign_in.html')
     
-    return render(request , 'sign_in.html')
+
+def log_user_out(request):
+    logout(request)
+    return redirect('sign_in')
