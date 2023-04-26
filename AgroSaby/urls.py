da7263c3ced06c114dd34a37beb7231a817856da
mohbaba <babamu09@gmail.com>
2023-04-26 13:47:01 +0100

    Adds dashboard templates and staticfiles

--- AgroSaby/urls.py
+++ AgroSaby/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path('user_profile/', include('user_profiles.urls')),
     path('products/', include('products.urls')),
-    path('admin_/', include('vendor_reg.urls'))
+    path('seller_admin_/', include('vendor_reg.urls'))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
